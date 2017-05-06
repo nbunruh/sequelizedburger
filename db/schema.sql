2573ee69a3a688441cef21102d99efce0e28bfbc
server <nolanbunruh@gmail.com>
2017-05-06 02:46:35 -0700

    test

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE burger_db;
+
+USE burger_db;
+
+CREATE TABLE burgers (
+	id INT NOT NULL AUTO_INCREMENT,
+	burger_name VARCHAR(50) NOT NULL,
+	devoured BOOLEAN default false,
+	PRIMARY KEY(id)
+);
